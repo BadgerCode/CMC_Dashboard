62427f54ea201e79d91d2ddb0720d43e1ee38e44
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-10-24 23:57:50 +0100

    Add pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import GalleryView from "@/views/GalleryView.vue";
+import HomeView from "@/views/HomeView.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/gallery",
+      name: "gallery",
+      component: GalleryView
+    }
+  ],
+});
 
-export default router
+export default router;
