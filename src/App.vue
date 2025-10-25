d63432880f8901e834f739469e3408d4c13b3290
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-10-25 19:20:13 +0100

    Fix image path

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
           <div class="flex p-4 items-center justify-between">
             <div class="flex items-center flex-wrap justify-center gap-4">
               <div>
-                <img class="h-8" src="/public/favicon.png"></img>
+                <img class="h-8" src="/favicon.png"></img>
               </div>
               <div class="shrink-0 text-white">
                 SMPOnline Dashboard (Unofficial)
