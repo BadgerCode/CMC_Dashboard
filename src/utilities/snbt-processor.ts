c300bb94ddac93e5b3125c4d7003541a81378397
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-11-20 20:20:36 +0000

    Quick fix for SNBT data

--- src/utilities/snbt-processor.ts
+++ src/utilities/snbt-processor.ts
@@ -9,12 +9,12 @@ export interface SNBTData {
 export function parseSNBTData(snbtData: string): SNBTData {
     let parsedData = {} as SNBTData;
 
-    parsedData.potionEffect = [...snbtData.matchAll(/.*{potion:"([^"]+)"}.*/gi)][0]?.[1];
-    parsedData.paintingID = [...snbtData.matchAll(/.*"xercapaint:canvas_id":"([a-fA-F0-9_\-]+)",.*/gi)][0]?.[1];
-    parsedData.paintingName = [...snbtData.matchAll(/.*"xercapaint:canvas_title":'?(.+)'?,\"xercapaint:canvas_version.*/gi)][0]?.[1];
+    // parsedData.potionEffect = [...snbtData.matchAll(/.*{potion:"([^"]+)"}.*/gi)][0]?.[1];
+    // parsedData.paintingID = [...snbtData.matchAll(/.*"xercapaint:canvas_id":"([a-fA-F0-9_\-]+)",.*/gi)][0]?.[1];
+    // parsedData.paintingName = [...snbtData.matchAll(/.*"xercapaint:canvas_title":'?(.+)'?,\"xercapaint:canvas_version.*/gi)][0]?.[1];
 
-    let rawEnchantments = [...snbtData.matchAll(/.*\"minecraft:enchantments\":{levels:{([^}]+)}}.*/gi)][0]?.[1]?.split(",") ?? [];
-    parsedData.enchantments = rawEnchantments.map(raw => raw.replace(/"minecraft:([^"]+)":(\d+)/, "$1 $2").replace(/_/g, " "));
+    // let rawEnchantments = [...snbtData.matchAll(/.*\"minecraft:enchantments\":{levels:{([^}]+)}}.*/gi)][0]?.[1]?.split(",") ?? [];
+    // parsedData.enchantments = rawEnchantments.map(raw => raw.replace(/"minecraft:([^"]+)":(\d+)/, "$1 $2").replace(/_/g, " "));
 
     return parsedData;
 }
