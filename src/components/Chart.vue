84b0d34e4a90e985d9d68923683f41aeb0226dac
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-11-21 03:05:30 +0000

    Loading spinner on player count page

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -2,7 +2,13 @@
 import { Config } from "@/config";
 import { formatDate } from "@/utilities/date-format";
 import Chart from "chart.js/auto";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+import Loading from "./Loading.vue";
+
+const loading = ref(true);
+
+const data = ref([] as ChartEntry[]);
+const currentDate = ref(new Date());
 
 interface ChartEntry {
   dateTime: Date;
@@ -12,18 +18,17 @@ interface ChartEntry {
 }
 
 onMounted(async () => {
-  let currentDate = new Date();
-  let startDate = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000); // 7 days ago
+  currentDate.value = new Date();
+  let startDate = new Date(currentDate.value.getTime() - 7 * 24 * 60 * 60 * 1000); // 7 days ago
 
   let playerCounts = (await getPlayerCounts(startDate)).sort((a, b) => a.dateTime.localeCompare(b.dateTime));
 
   // Generate data points for each interval
   let intervalMs = 1 * 60 * 60 * 1000; // Every 1 hour
-  let numDataPoints = (currentDate.getTime() - startDate.getTime()) / intervalMs;
+  let numDataPoints = (currentDate.value.getTime() - startDate.getTime()) / intervalMs;
 
-  let data = [] as ChartEntry[];
   for (let i = 0; i < numDataPoints; i++) {
-    data.push({
+    data.value.push({
       dateTime: new Date(startDate.getTime() + i * intervalMs),
       index: i,
       playerCounts: [],
@@ -32,9 +37,9 @@ onMounted(async () => {
   }
 
   // Add a final entry for the current time
-  data.push({
-    dateTime: currentDate,
-    index: data.length,
+  data.value.push({
+    dateTime: currentDate.value,
+    index: data.value.length,
     playerCounts: [],
     currentTime: true,
   });
@@ -42,18 +47,37 @@ onMounted(async () => {
   for (const playerCount of playerCounts) {
     let targetIndex = Math.round((new Date(playerCount.dateTime).getTime() - startDate.getTime()) / intervalMs);
 
-    if (data[targetIndex] == undefined) {
-      console.log("Failed to get valid index", playerCount, targetIndex, data.length);
+    if (data.value[targetIndex] == undefined) {
+      console.log("Failed to get valid index", playerCount, targetIndex, data.value.length);
       continue;
     }
 
-    data[targetIndex].playerCounts.push(playerCount.count);
+    data.value[targetIndex].playerCounts.push(playerCount.count);
   }
 
-  new Chart(document.getElementById("chartOutput") as HTMLCanvasElement, {
+  renderChart(document.getElementById("chartOutput"));
+  loading.value = false;
+});
+
+interface PlayerCount {
+  dateTime: string;
+  count: number;
+}
+
+async function getPlayerCounts(startDate: Date): Promise<PlayerCount[]> {
+  let httpResponse = await fetch(`${Config.APIURL}/api/serverstats/playercounts?date=${startDate.toISOString()}`, { method: "get" });
+
+  if (httpResponse.status !== 200) return [];
+
+  let response = await httpResponse.json();
+  return response.items;
+}
+
+function renderChart(canvas: Element | any) {
+  new Chart(canvas as HTMLCanvasElement, {
     type: "line",
     data: {
-      labels: data.map((row) => row.dateTime.getTime()),
+      labels: data.value.map((row) => row.dateTime.getTime()),
       datasets: [
         {
           label: "Players",
@@ -61,7 +85,7 @@ onMounted(async () => {
           borderWidth: 1,
           pointRadius: 2,
           pointStyle: "circle",
-          data: data.map((row) => {
+          data: data.value.map((row) => {
             // Empty results for times without player counts
             if (row.playerCounts.length == 0) return NaN;
 
@@ -94,7 +118,7 @@ onMounted(async () => {
           },
           type: "linear",
           // min: startDate.getTime(),
-          max: currentDate.getTime(),
+          max: currentDate.value.getTime(),
           ticks: {
             stepSize: 6 * 60 * 60 * 1000,
             callback: function (value, index, ticks) {
@@ -112,24 +136,11 @@ onMounted(async () => {
       },
     },
   });
-});
-
-interface PlayerCount {
-  dateTime: string;
-  count: number;
-}
-
-async function getPlayerCounts(startDate: Date): Promise<PlayerCount[]> {
-  let httpResponse = await fetch(`${Config.APIURL}/api/serverstats/playercounts?date=${startDate.toISOString()}`, { method: "get" });
-
-  if (httpResponse.status !== 200) return [];
-
-  let response = await httpResponse.json();
-  return response.items;
 }
 </script>
 
 <template>
+  <Loading v-if="loading" :fill-space="true"></Loading>
   <div class="min-h-[400px]">
     <canvas id="chartOutput"></canvas>
   </div>
