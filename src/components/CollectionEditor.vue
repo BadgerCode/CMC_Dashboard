2a17ad103561c70b64a683d3f65b85597e0b5984
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-11-24 11:30:44 +0000

    Implement moving paintings in collection

--- src/components/CollectionEditor.vue
+++ src/components/CollectionEditor.vue
@@ -23,16 +23,49 @@ onUpdated(() => {
   initFlowbite(); // Include on any component where you need flowbite JS functionality
 });
 
-const paintingColumns = computed(() => {
-  let columns = [] as Painting[][];
+const paintingRows = computed(() => {
+  let rows = [] as Painting[][];
   const chunkSize = numCols.value;
   for (let i = 0; i < props.paintings.length; i += chunkSize) {
     const chunk = props.paintings.slice(i, i + chunkSize);
-    columns.push(chunk);
+    rows.push(chunk);
   }
 
-  return columns;
+  return rows;
 });
+
+function move(paintingId: string, direction: string) {
+  let currentIndex = props.paintings.findIndex(p => p.id == paintingId); // 0-based
+  if (currentIndex === -1) {
+    console.error(`Painting '${paintingId}' not found`);
+    return;
+  }
+
+  let newIndex = currentIndex;
+  if (direction == "left") {
+    newIndex -= 1;
+  }
+  else if (direction == "right") {
+    newIndex += 1;
+  }
+  else if (direction == "up") {
+    newIndex -= numCols.value;
+  }
+  else if (direction == "down") {
+    newIndex += numCols.value;
+  }
+  else {
+    return;
+  }
+
+  if (newIndex < 0 || newIndex >= props.paintings.length) return;
+
+  // Swap neighbour and painting
+  let painting = props.paintings[currentIndex]!;
+  let neighbour = props.paintings[newIndex]!;
+  props.paintings[currentIndex] = neighbour;
+  props.paintings[newIndex] = painting;
+}
 
 async function saveCollection() {
   // TODO: Save
@@ -56,14 +89,12 @@ async function saveCollection() {
           <span class="mb-1 text-base font-medium text-heading">Collection Editor</span>
           <div class="mb-3">{{ paintings.length }} paintings selected</div>
           <div class="grid grid-cols-2 gap-3">
-            <button type="button" data-dismiss-target="#toast-interactive"
-              class="w-full text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-xs px-3 py-1.5 focus:outline-none"
+            <button type="button" data-dismiss-target="#toast-interactive" class="w-full button-secondary"
               @click="$emit('clear')">
               Clear
             </button>
-            <button type="button"
-              class="w-full inline-flex items-center justify-center text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-xs px-3 py-1.5 focus:outline-none"
-              data-modal-target="collection-editor-modal" data-modal-show="collection-editor-modal">
+            <button type="button" class="w-full button-primary" data-modal-target="collection-editor-modal"
+              data-modal-show="collection-editor-modal">
               Create
             </button>
           </div>
@@ -97,13 +128,38 @@ async function saveCollection() {
 
             <!-- Paintings -->
             <div>
-              <div v-for="column in paintingColumns" class="flex flex-row flex-wrap w-full justify-center">
-                <div v-for="painting in column" class="flex justify-center items-center flex-col">
+              <div v-for="row in paintingRows" class="flex flex-row flex-wrap w-full justify-center">
+                <div v-for="painting in row" class="flex justify-center items-center flex-col">
                   <div class="painting relative h-[192px] w-[192px]">
                     <RenderPainting :painting-id="painting.id"></RenderPainting>
 
-                    <div class="overlay absolute top-0 left-0 right-0 bg-gray-950/60">{{ painting.title }}</div>
-                    <div class="overlay absolute bottom-0 left-0 right-0 bg-gray-950/60">{{ painting.authorName }}</div>
+                    <div
+                      class="overlay absolute top-0 bottom-0 left-0 right-0 flex flex-col bg-gray-950/60 justify-between text-xs">
+                      <div>{{ painting.title }}</div>
+
+                      <div class="flex flex-row justify-center">
+                        <button type="button" class="button-secondary" @click="move(painting.id, 'up')">
+                          <font-awesome-icon icon="fa-solid fa-arrow-up" class="rotate-0" />
+                        </button>
+                      </div>
+
+                      <div class="flex flex-row justify-center">
+                        <button type="button" class="button-secondary" @click="move(painting.id, 'left')">
+                          <font-awesome-icon icon="fa-solid fa-arrow-up" class="rotate-270" />
+                        </button>
+                        <button type="button" class="button-secondary" @click="move(painting.id, 'right')">
+                          <font-awesome-icon icon="fa-solid fa-arrow-up" class="rotate-90" />
+                        </button>
+                      </div>
+
+                      <div class="flex flex-row justify-center">
+                        <button type="button" class="button-secondary" @click="move(painting.id, 'down')">
+                          <font-awesome-icon icon="fa-solid fa-arrow-up" class="rotate-180" />
+                        </button>
+                      </div>
+
+                      <div class="text-xs">{{ painting.authorName }}</div>
+                    </div>
                   </div>
                 </div>
               </div>
