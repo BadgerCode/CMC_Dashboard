7e8f20828e70e9121d8d69e0339efc347ff813d8
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-11-26 18:58:06 +0000

    Add collection sorting by simple number

--- src/components/CollectionEditor.vue
+++ src/components/CollectionEditor.vue
@@ -29,11 +29,11 @@ const widthBlocks = ref(8);
 const startAtBottom = ref(false);
 const sortOptions = [
   { text: "Order of selection", value: "selection" },
-  { text: "Painting Name 1/x", value: "fractions" }
+  { text: "Painting Name 1", value: "simpleNumber" },
+  { text: "Painting Name 1/x", value: "fractions" },
 ] as DropdownOption[];
 const sortOrder = ref(sortOptions[0]!.value);
-const sortOrderText = computed(() => sortOptions.find(o => o.value == sortOrder.value)?.text);
-
+const sortOrderText = computed(() => sortOptions.find((o) => o.value == sortOrder.value)?.text);
 
 onMounted(async () => {
   initFlowbite(); // Include on any component where you need flowbite JS functionality
@@ -58,42 +58,54 @@ function startEditor() {
 }
 
 const fractionNameRegex = new RegExp(/.*[^\d](\d+)\/\d+.*/);
+const simpleNumberNameRegex = new RegExp(/.*[^\d](\d+)[\s.]*/);
 function arrangePaintings() {
-  // Setup positions
-  let x = 0;
-  let y = 0;
-
   // Order paintings
   let orderedPaintings = props.paintings.slice();
-  if (sortOrder.value == "fractions") orderedPaintings.sort((a, b) => {
-    let aPosition = parseInt(a.title.match(fractionNameRegex)?.[1] ?? "");
-    let bPosition = parseInt(b.title.match(fractionNameRegex)?.[1] ?? "");
+  if (sortOrder.value == "simpleNumber" || sortOrder.value == "fractions") {
+    let regex = simpleNumberNameRegex;
+    if (sortOrder.value == "fractions") regex = fractionNameRegex;
 
-    // TODO: Handle just aPosition or just bPosition being null/not a number
-    if (Number.isNaN(aPosition) || Number.isNaN(bPosition))
-      return a.title.localeCompare(b.title);
+    orderedPaintings.sort((a, b) => {
+      let aPosition = parseInt(a.title.match(regex)?.[1] ?? "");
+      let bPosition = parseInt(b.title.match(regex)?.[1] ?? "");
 
-    return aPosition - bPosition;
-  });
+      // TODO: Handle just aPosition or just bPosition being null/not a number
+      if (Number.isNaN(aPosition) || Number.isNaN(bPosition)) return a.title.localeCompare(b.title);
+
+      return aPosition - bPosition;
+    });
+  }
 
   // Determine positions
+  let x = 0;
+  let y = 0;
+  let minY = 0;
   let rowHeight = 1;
-  let paintingHeight = 0;
   for (const painting of orderedPaintings) {
     positions.value[painting.id] = `${x},${y}`;
 
     let size = painting.size.toLowerCase();
     x += size == "large" || size == "wide" ? 2 : 1;
     rowHeight = Math.max(rowHeight, size == "large" || size == "tall" ? 2 : 1);
+    minY = Math.min(minY, y);
 
     if (x >= widthBlocks.value) {
-      paintingHeight += rowHeight;
       x = 0;
-      y += rowHeight;
+      y += !startAtBottom.value ? rowHeight : -rowHeight;
     }
   }
 
-  // TODO: Implement startAtBottom
+  // Top-to-bottom reordering
+  if (startAtBottom.value && y < 0) {
+    let offset = minY * -1; // Account for the final row
+
+    for (const painting of orderedPaintings) {
+      let pos = positions.value[painting.id]!.split(",");
+      let updatedY = parseInt(pos[1]!) + offset;
+      positions.value[painting.id] = `${pos[0]},${updatedY}`;
+    }
+  }
 }
 
 function getStyle(painting: Painting) {
@@ -101,7 +113,7 @@ function getStyle(painting: Painting) {
 
   return {
     transform: `translate(${toNumber(pos[0]) * 96}px, ${toNumber(pos[1]) * 96}px)`,
-    "transform-origin": "top left",
+    "transform-origin": startAtBottom.value == false ? "top left" : "bottom left",
   };
 }
 
@@ -158,7 +170,8 @@ async function saveCollection() {
 <template>
   <div>
     <!-- Toast notification -->
-    <div id="toast-interactive"
+    <div
+      id="toast-interactive"
       class="fixed z-100 bottom-5 right-5 space-y-4 p-3 text-body bg-neutral-primary-soft rounded-base shadow-xs border border-default"
       role="alert">
       <div class="flex">
@@ -170,8 +183,7 @@ async function saveCollection() {
           <span class="mb-1 text-base font-medium text-heading">Collection Editor</span>
           <div class="mb-3">{{ paintings.length }} paintings selected</div>
           <div class="grid grid-cols-2 gap-3">
-            <button type="button" data-dismiss-target="#toast-interactive" class="w-full button-secondary"
-              @click="$emit('clear')">
+            <button type="button" data-dismiss-target="#toast-interactive" class="w-full button-secondary" @click="$emit('clear')">
               Clear
             </button>
             <button type="button" class="w-full button-primary" @click="startEditor()">Create</button>
@@ -181,12 +193,16 @@ async function saveCollection() {
     </div>
 
     <!-- Editor modal -->
-    <div id="collection-editor-modal" v-if="showModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+    <div
+      id="collection-editor-modal"
+      v-if="showModal"
+      data-modal-backdrop="static"
+      tabindex="-1"
+      aria-hidden="true"
       class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-200 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-950/60">
       <div class="relative p-4 w-full max-h-full">
         <!-- Modal content -->
-        <div
-          class="flex flex-col max-h-full relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
+        <div class="flex flex-col max-h-full relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
           <!-- Modal header -->
           <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
             <h3 class="text-lg font-medium text-heading">Collection Editor</h3>
@@ -198,8 +214,7 @@ async function saveCollection() {
               <!-- Title -->
               <div class="max-w-sm mx-auto">
                 <label class="block mb-2.5 text-sm font-medium text-heading">Title</label>
-                <input type="text" class="textbox w-80" placeholder="Painting title (e.g. shulker box name)"
-                  v-model="title" />
+                <input type="text" class="textbox w-80" placeholder="Painting title (e.g. shulker box name)" v-model="title" />
               </div>
 
               <!-- Author -->
@@ -212,14 +227,15 @@ async function saveCollection() {
             <div class="flex flex-row">
               <!-- Paintings -->
               <div class="flex-1 relative z-0">
-                <div v-for="painting in paintings"
-                  class="flex absolute justify-center items-center flex-col cursor-pointer" :style="getStyle(painting)"
+                <div
+                  v-for="painting in paintings"
+                  class="flex absolute justify-center items-center flex-col cursor-pointer"
+                  :style="getStyle(painting)"
                   @click="selectedPaintingId = painting.id">
                   <div class="painting relative" :class="painting.size.toLowerCase()">
                     <RenderPainting :painting-id="painting.id"></RenderPainting>
 
-                    <div
-                      class="overlay absolute top-0 bottom-0 left-0 right-0 flex flex-col bg-gray-950/60 justify-between text-xs">
+                    <div class="overlay absolute top-0 bottom-0 left-0 right-0 flex flex-col bg-gray-950/60 justify-between text-xs">
                       <div>{{ painting.title }}</div>
 
                       <div class="text-xs">{{ painting.authorName }}</div>
@@ -230,80 +246,84 @@ async function saveCollection() {
 
               <!-- SIDE BAR -->
               <div class="z-100 p-2 w-[300px]">
-
                 <div id="collection-controls" data-accordion="collapse" class="accordion">
-
                   <!-- Automatic ordering -->
                   <h2 id="collection-controls-auto-heading">
-                    <button type="button" class="accordion-header-button"
-                      data-accordion-target="#collection-controls-auto" aria-expanded="true"
+                    <button
+                      type="button"
+                      class="accordion-header-button"
+                      data-accordion-target="#collection-controls-auto"
+                      aria-expanded="true"
                       aria-controls="collection-controls-auto">
                       <span>Automatic</span>
-                      <font-awesome-icon data-accordion-icon icon="fa-solid fa-angle-up"
-                        class="w-5 h-5 rotate-180 shrink-0" />
+                      <font-awesome-icon data-accordion-icon icon="fa-solid fa-angle-up" class="w-5 h-5 rotate-180 shrink-0" />
                     </button>
                   </h2>
 
-                  <div id="collection-controls-auto" class="accordion-body hidden"
-                    aria-labelledby="collection-controls-auto-heading">
+                  <div id="collection-controls-auto" class="accordion-body hidden" aria-labelledby="collection-controls-auto-heading">
                     <div class="text-body flex flex-col gap-4">
-
                       <!-- Number of columns -->
                       <div class="flex flex-col gap-2">
-                        <label class="block mb-2.5 text-sm font-medium text-heading">
-                          Total width (blocks)
-                        </label>
-                        <input type="text" class="textbox w-full" placeholder="E.g. 2 large paintings is 4 blocks"
-                          v-model="widthBlocks" />
+                        <label class="block mb-2.5 text-sm font-medium text-heading"> Total width (blocks) </label>
+                        <input type="text" class="textbox w-full" placeholder="E.g. 2 large paintings is 4 blocks" v-model="widthBlocks" />
                       </div>
 
                       <!-- Sorting -->
                       <div class="flex flex-col gap-2">
-                        <label class="block text-sm font-medium text-heading">
-                          Sort by: {{ sortOrderText }}
-                        </label>
+                        <label class="block text-sm font-medium text-heading"> Sort by: {{ sortOrderText }} </label>
 
-                        <DropdownFilter :placeholder="'Sort by'" :icon="'fa-solid fa-arrow-down-1-9'"
-                          :options="sortOptions" v-model="sortOrder" :single-selection="true">
+                        <DropdownFilter
+                          :placeholder="'Sort by'"
+                          :icon="'fa-solid fa-arrow-down-1-9'"
+                          :options="sortOptions"
+                          v-model="sortOrder"
+                          :single-selection="true">
                         </DropdownFilter>
 
-                        <!-- <Checkbox :label="'Start at bottom'" v-model="startAtBottom"></Checkbox> -->
+                        <Checkbox :label="'Start at bottom'" v-model="startAtBottom"></Checkbox>
                       </div>
 
                       <!-- Apply -->
                       <div class="flex flex-row gap-2 justify-center">
-                        <button @click="arrangePaintings()" type="button" class="button-secondary mt-4">
-                          Apply
-                        </button>
+                        <button @click="arrangePaintings()" type="button" class="button-secondary mt-4">Apply</button>
                       </div>
                     </div>
                   </div>
 
                   <!-- Manual ordering -->
                   <h2 id="collection-controls-manual-heading">
-                    <button type="button" class="accordion-header-button"
-                      data-accordion-target="#collection-controls-manual" aria-expanded="false"
+                    <button
+                      type="button"
+                      class="accordion-header-button"
+                      data-accordion-target="#collection-controls-manual"
+                      aria-expanded="false"
                       aria-controls="collection-controls-manual">
                       <span>Manual</span>
-                      <font-awesome-icon data-accordion-icon icon="fa-solid fa-angle-up"
-                        class="w-5 h-5 rotate-180 shrink-0" />
+                      <font-awesome-icon data-accordion-icon icon="fa-solid fa-angle-up" class="w-5 h-5 rotate-180 shrink-0" />
                     </button>
                   </h2>
 
-                  <div id="collection-controls-manual" class="accordion-body hidden"
-                    aria-labelledby="collection-controls-manual-heading">
+                  <div id="collection-controls-manual" class="accordion-body hidden" aria-labelledby="collection-controls-manual-heading">
                     <div class="text-body">
                       <!-- Positions -->
                       <div class="max-h-[400px] overflow-x-scroll">
                         <div v-for="painting in paintings" class="mb-2">
                           <!-- Painting name -->
-                          <label :for="`painting-pos-${painting.id}`" class="block mb-1 text-sm font-medium"
-                            :class="{ 'text-blue-600': selectedPaintingId == painting.id, 'text-heading': selectedPaintingId != painting.id }">
+                          <label
+                            :for="`painting-pos-${painting.id}`"
+                            class="block mb-1 text-sm font-medium"
+                            :class="{
+                              'text-blue-600': selectedPaintingId == painting.id,
+                              'text-heading': selectedPaintingId != painting.id,
+                            }">
                             {{ painting.title }}
                           </label>
 
                           <!-- Position -->
-                          <input type="text" :id="`painting-pos-${painting.id}`" v-model="positions[painting.id]"
+                          <input
+                            type="text"
+                            :id="`painting-pos-${painting.id}`"
+                            v-model="positions[painting.id]"
                             class="block px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" />
                         </div>
                       </div>
@@ -316,12 +336,8 @@ async function saveCollection() {
 
           <!-- Modal footer -->
           <div class="flex items-center justify-end border-t border-default space-x-4 pt-4 md:pt-5">
-            <button type="button" class="button-primary" @click="saveCollection()">
-              Save
-            </button>
-            <button @click="showModal = false" type="button" class="button-secondary">
-              Cancel
-            </button>
+            <button type="button" class="button-primary" @click="saveCollection()">Save</button>
+            <button @click="showModal = false" type="button" class="button-secondary">Cancel</button>
           </div>
         </div>
       </div>
