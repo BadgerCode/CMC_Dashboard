4987691f1538c6e948f0649f848806cbcc42e62d
Michael Hawkins <BadgerCode@users.noreply.github.com>
2026-03-31 17:31:33 +0100

    Load custom disc stats from API

--- src/api/customDiscs/api.ts
+++ src/api/customDiscs/api.ts
@@ -1,15 +1,36 @@
 import { Config } from "@/config";
-import type { CustomDiscsOverview } from "./customDiscs";
+import type { CustomDiscsOverview, CustomDiscStats } from "./customDiscs";
 import { itemsStore } from "@/store/items-state";
 
 export async function retrieveCustomDiscs(): Promise<CustomDiscsOverview> {
   // Don't reload if data is recent
-  if (itemsStore.customDiscs != null) return itemsStore.customDiscs;
+  if (itemsStore.customDiscs != null && new Date().getTime() - itemsStore.customDiscsLastUpdated.getTime() < 30000)
+    return itemsStore.customDiscs;
 
   // Get custom discs
   let httpResponse = await fetch(`${Config.APIURL}/api/v2/customDiscs`, { method: "get" });
   if (httpResponse.status !== 200) return { discs: [], unsoldDiscs: 0 };
 
   itemsStore.customDiscs = (await httpResponse.json()).result as CustomDiscsOverview;
+  itemsStore.customDiscsLastUpdated = new Date();
   return itemsStore.customDiscs;
+}
+
+export async function retrieveCustomDiscStats(): Promise<{ [discName: string]: CustomDiscStats; }> {
+  // Don't reload if data is recent
+  if (itemsStore.customDiscStats != null && new Date().getTime() - itemsStore.customDiscsStatsLastUpdated.getTime() < 30000)
+    return itemsStore.customDiscStats;
+
+  // Get custom disc stats
+  let httpResponse = await fetch(`${Config.APIURL}/api/customDiscs/stats`, { method: "get" });
+  if (httpResponse.status !== 200) return {};
+
+  let discs = (await httpResponse.json()).result.discs as CustomDiscStats[];
+  itemsStore.customDiscStats = {};
+  for (const disc of discs) {
+    itemsStore.customDiscStats[disc.discName] = disc;
+  }
+
+  itemsStore.customDiscsStatsLastUpdated = new Date();
+  return itemsStore.customDiscStats;
 }
--- src/store/items-state.ts
+++ src/store/items-state.ts
@@ -1,15 +1,21 @@
-import type { CustomDiscsOverview } from "@/api/customDiscs/customDiscs";
+import type { CustomDiscsOverview, CustomDiscStats } from "@/api/customDiscs/customDiscs";
 import type { VillagerTrade } from "@/api/villagerTrades/villagerTrades";
 import { reactive } from "vue";
 
 export interface ItemsOverview {
   customDiscs: CustomDiscsOverview | null;
+  customDiscsLastUpdated: Date;
+  customDiscStats: { [discName: string]: CustomDiscStats; };
+  customDiscsStatsLastUpdated: Date;
   villagerTrades: VillagerTrade[];
   villagerTradesLastUpdated: Date;
 }
 
 export const itemsStore = reactive({
   customDiscs: null,
+  customDiscsLastUpdated: new Date(0),
+  customDiscStats: {},
+  customDiscsStatsLastUpdated: new Date(0),
   villagerTrades: [],
   villagerTradesLastUpdated: new Date(0),
 } as ItemsOverview);
